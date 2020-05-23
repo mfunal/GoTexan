7b1cebb6af356577201af4ed5fecd610a1145dac
mfunal <64172247+mfunal@users.noreply.github.com>
2020-05-23 11:46:05 -0500

    4th Update

--- src/GoTexan_Practice_day_05_23_20/QuestionNumber_03.java
+++ src/GoTexan_Practice_day_05_23_20/QuestionNumber_03.java
@@ -17,15 +17,15 @@ input 1,
 output: [1]
  */
 public static void main(String[] args) {
-    int[] num = {3,4};
+    int[] num = {3,4,4,6,7,8,9};
     int []arr1= new int[2];
     int []arr2= new int[1];
-    if(num.length>2){
+    if(num.length>2){// {3,4,4,6,7,8,9}; result =[3],[4]
         arr1[0]=num[0];
         arr1[1]=num[1];
         System.out.println(Arrays.toString(arr1));
 
-    }else if(num.length<=2){
+    }else if(num.length<=2){//{3,4}; result =[3].
         arr2[0]=num[0];
         System.out.println(Arrays.toString(arr2));
 
