fe2da2c1d773c7683d3d19648595335df8d230fb
mfunal <64172247+mfunal@users.noreply.github.com>
2020-05-23 00:32:22 -0500

    2nd Update

--- src/GoTexan_Practice_day_05_23_20/QuestionNumber_24/_24_SubClass.java
+++ src/GoTexan_Practice_day_05_23_20/QuestionNumber_24/_24_SubClass.java
@@ -1,4 +1,9 @@
 package GoTexan_Practice_day_05_23_20.QuestionNumber_24;
 
 public class _24_SubClass {
+//    get variables and methods from parent class
+//    create studentID and grade variables
+//    create an object
+//    print out the object
+
 }
--- src/GoTexan_Practice_day_05_23_20/QuestionNumber_24/_24_SuperClass.java
+++ src/GoTexan_Practice_day_05_23_20/QuestionNumber_24/_24_SuperClass.java
@@ -1,4 +1,7 @@
 package GoTexan_Practice_day_05_23_20.QuestionNumber_24;
 
 public class _24_SuperClass {
+    // Please create a personal info super class
+    // create variables: name, surname, age, gender
+    // create setInfo method, and String toSting method
 }
